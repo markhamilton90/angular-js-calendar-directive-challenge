06a177b238aacaf2b666cd120272f0559a87212d
markhamilton90 <msh387@gmail.com>
2016-01-14 14:55:21 -0600

    initial

--- app/app.js
+++ app/app.js
@@ -1,3 +1,30 @@
-angular.module('calendarDemoApp', []);
 
-// your controller and directive code go here
+angular.module('calendarDemoApp', [])
+	.controller('CalendarCtrl', function($scope, $element) {
+		//var month = $element.find("select")[0];
+		//var year = $element.find("select")[1];
+
+		$scope.monthOptions = [
+			"January", "February", "March", "April", "May", "June", "July",
+			"August", "September", "October", "November", "December"
+		]
+
+		$scope.yearOptions = [];
+		for (var i = 1996; i < 2037; i++) {
+			$scope.yearOptions.push(i);
+		}
+
+		$scope.change = function() {
+			console.log($scope.confirmMonth);
+		}
+
+		var ctrl = this;
+	})
+	.directive('calendar', function() {
+		return {
+			restrict: 'E',
+			templateUrl: 'calendar.html',
+			scope: true,
+			
+		}
+	})
